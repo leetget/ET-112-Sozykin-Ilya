24fbff815b1c3f9383050bce1a218b0e8d7da5c9
not animal <sozykin25@gmail.com>
2023-05-02 15:09:37 +0500

    final

--- sun.cpp
+++ sun.cpp
@@ -1,6 +1,9 @@
 #include "graphics.h"
 
-void sun(int x, int y, int r){
+void sun(){
+   int x = 50;
+   int y = 50;
+   int r = 40;
    setcolor(YELLOW);
    setfillstyle(SOLID_FILL, YELLOW);
    fillellipse(x, y, r, r);
