54c15879dbbcb8a8db5128cc495a5394f4cc47b4
leetget <96439185+leetget@users.noreply.github.com>
2023-05-02 16:47:52 +0500

    finalfinal(without frence)

--- program.cpp
+++ program.cpp
@@ -3,17 +3,11 @@
 int main()
 { 
  initwindow(800,600);
-<<<<<<< Updated upstream
- house(600,250,190); // дом
- sun(50,50,50); // солнце
- man(); // человек
- fence(); // забор
-=======
+
  house(); // дом
  sun(); // солнце
  //man(); // человек
  //fence(); // забор
->>>>>>> Stashed changes
  getch();
  closegraph();
 }
