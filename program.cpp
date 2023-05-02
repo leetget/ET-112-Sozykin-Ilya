b291d1ef1309e289e473254fb55ef8329db12c86
not animal <sozykin25@gmail.com>
2023-05-02 15:04:33 +0500

    hope final

--- program.cpp
+++ program.cpp
@@ -3,10 +3,10 @@
 int main()
 { 
  initwindow(800,600);
- house(); // дом
- sun(); // солнце
- man(); // человек
- fence(); // забор
+ house(600,250,190); // дом
+ sun(50,50,50); // солнце
+ //man(); // человек
+ //fence(); // забор
  getch();
  closegraph();
 }
--- sun.cpp
+++ sun.cpp
@@ -1,6 +1,9 @@
 #include "graphics.h"
 
-void sun(int x, int y, int r){
+void sun(){
+   int x = 60;
+   int y = 50;
+   int r = 50;
    setcolor(YELLOW);
    setfillstyle(SOLID_FILL, YELLOW);
    fillellipse(x, y, r, r);
